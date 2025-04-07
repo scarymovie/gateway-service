e7ddcab6bc3ffba7684178811211e79438b154f5
evgeniy <evgeny.vinokurov@yoso.ru>
2025-04-07 23:00:14 +0300

    init

---
+++ gateway-service/go.mod
@@ -0,0 +1,25 @@
+module gateway-service
+
+go 1.24
+
+require (
+	github.com/labstack/echo/v4 v4.13.3
+	github.com/oapi-codegen/runtime v1.1.1
+	google.golang.org/grpc v1.71.1
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
+)
---
+++ gateway-service/internal/handler/handler.go
@@ -0,0 +1,58 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	external "gateway-service/external"
+	proto "gateway-service/external/proto"
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct {
+	AuthClient proto.AuthServiceClient
+}
+
+func (h *Handler) CreateUser(ctx echo.Context) error {
+	var req external.CreateUserRequest
+	if err := ctx.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid request body: "+err.Error())
+	}
+
+	resp, err := h.AuthClient.CreateUser(
+		context.Background(),
+		&proto.CreateUserRequest{Name: req.Name},
+	)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "CreateUser failed: "+err.Error())
+	}
+
+	// Формируем ответ, используя тип User из openapi.gen.go
+	user := external.User{
+		Id:   &resp.User.Id,
+		Name: &resp.User.Name,
+	}
+
+	return ctx.JSON(http.StatusCreated, user)
+}
+
+func (h *Handler) GetUser(ctx echo.Context, id string) error {
+	resp, err := h.AuthClient.GetUserByID(
+		context.Background(),
+		&proto.GetUserRequest{Id: id},
+	)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "GetUserByID failed: "+err.Error())
+	}
+
+	if resp.User == nil {
+		return echo.NewHTTPError(http.StatusNotFound, "User not found")
+	}
+
+	user := external.User{
+		Id:   &resp.User.Id,
+		Name: &resp.User.Name,
+	}
+
+	return ctx.JSON(http.StatusOK, user)
+}
